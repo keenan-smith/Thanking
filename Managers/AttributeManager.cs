8ed4c33ccbb32a62db4cb09cbac35ac2133c80c0
DefCon42 <klrehm123@gmail.com>
2017-10-03 15:37:57 -0400

    Actually load overrides and other shit now lol

--- Managers/AttributeManager.cs
+++ Managers/AttributeManager.cs
@@ -13,27 +13,8 @@ namespace Thanking.Managers
         public static void Init()
         {
             Debug.Log("Initializing attribute manager...");
-            foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
-            {
-                Attribute[] attributes = Attribute.GetCustomAttributes(type);
-
-                for (int i = 0; i < attributes.Length; i++)
-                {
-                    Attribute attribute = attributes[i];
-                    
-                    if (attribute is OverrideAttribute)
-                    {
-                        OverrideAttribute a = (OverrideAttribute) attribute;
-                        OverrideManager.OverrideClass(a.OverrideClass, type);
-                        return;
-                    }
-
-                    if (attribute is ComponentAttribute)
-                        LOADING.Loader.HookObject.AddComponent(type);
-                    
-                    //more shit to come here probably
-                }
-            }
+            ComponentManager.Load();
+            OverrideManager.Load();
         }
     }
 }
---
+++ Managers/ComponentManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Thanking.Attributes;
+
+namespace Thanking.Managers
+{
+    public static class ComponentManager
+    {
+        public static void Load()
+        {
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+                foreach (Type tClass in asm.GetTypes())
+                    if (tClass.IsClass)
+                        foreach (MethodInfo method in tClass.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance))
+                            if (Attribute.GetCustomAttribute(method, typeof(OverrideAttribute)) != null)
+                                LOADING.Loader.HookObject.AddComponent(tClass);]
+        }
+    }
+}
