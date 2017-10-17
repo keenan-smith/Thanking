9897da924ab3b1211eb4bc86a7918c7c029326ff
Unknown <Kr4kenKT@gmail.com>
2017-10-16 21:11:32 -0500

    Basic Config

---
+++ ThankingHack/Config/Manager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Thanking.Config
+{
+    public class Manager
+    {
+        public static Dictionary<String, object> CollectConfig()
+        {
+            Dictionary<String, object> ConfigFields = new Dictionary<string, object>();
+            
+            Type[] Types = Assembly.GetExecutingAssembly().GetTypes();
+
+            for (int i = 0; i < Types.Length; i++)
+            {
+                Type Type = Types[i];
+                FieldInfo[] Fields = Type.GetFields();
+
+                for (int o = 0; o < Fields.Length; o++)
+                {
+                    FieldInfo Field = Fields[o];
+                    
+                    if(Field.IsDefined(typeof(Save), false))
+                        ConfigFields.Add(Field.Name, Field.GetValue(null));
+                }
+            }
+
+            return ConfigFields;
+        }
+
+        public static void SaveConfig(Dictionary<String, object> Config)
+        {
+            String Path = $"{Application.dataPath}/Thanking.config";
+
+            File.WriteAllText(Path, JsonUtility.ToJson(Config));
+        }
+
+        public static void LoadConfig(Dictionary<String, object> Config)
+        {
+            Type[] Types = Assembly.GetExecutingAssembly().GetTypes();
+
+            for (int i = 0; i < Types.Length; i++)
+            {
+                Type Type = Types[i];
+                FieldInfo[] Fields = Type.GetFields();
+
+                for (int o = 0; o < Fields.Length; o++)
+                {
+                    FieldInfo Field = Fields[o];
+                    
+                    if(Field.IsDefined(typeof(Save), false) && Config.ContainsKey(Field.Name))
+                        Field.SetValue(null, Config[Field.Name]);
+                }
+            }
+        }
+    }
+}
