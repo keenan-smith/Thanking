fd9bc38a97d6863eb4b14b74f1fc2a4cac5abc35
Keenan Smith <01573187@student.commnet.edu>
2017-11-04 19:36:34 -0400

    fixed options

--- ThankingHack/Options/InteractionOptions.cs
+++ ThankingHack/Options/InteractionOptions.cs
@@ -5,9 +5,9 @@ namespace Thanking.Options
 	public static class InteractionOptions
 	{
 		[Save] public static bool HitStructures = false;
-		[Save] public static bool HitBarricades = false;
+		[Save] public static bool HitBarricades = true;
 		[Save] public static bool HitItems = true;
-		[Save] public static bool HitVehicles = false;
+		[Save] public static bool HitVehicles = true;
 		[Save] public static bool HitResources = true;
 	}
 }
