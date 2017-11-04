e7c087a76037fec9c3b7167fef712c0465a99c8c
Keenan Smith <01573187@student.commnet.edu>
2017-11-04 19:22:21 -0400

    started item filter tab

---
+++ ThankingHack/Components/UI/Menu/Tabs/ItemFilterTab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Thanking.Components.UI.Menu.Tabs
+{
+    public static class ItemFilterTab
+    {
+        public static void Tab()
+        {
+
+        }
+    }
+}
