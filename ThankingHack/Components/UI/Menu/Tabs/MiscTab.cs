a6ccc06b4706e19f5ad88ecdff4e0cd232cbea4d
Keenan Smith <01573187@student.commnet.edu>
2017-11-04 23:18:59 -0400

    finished weapon menu

--- ThankingHack/Components/UI/Menu/Tabs/MiscTab.cs
+++ ThankingHack/Components/UI/Menu/Tabs/MiscTab.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Thanking.Options;
+using UnityEngine;
 
 namespace Thanking.Components.UI.Menu.Tabs
 {
@@ -9,7 +11,27 @@ namespace Thanking.Components.UI.Menu.Tabs
     {
         public static void Tab()
         {
-
+            Prefab.MenuArea(new Rect(0, 0, 466, 436), "MISC", () =>
+            {
+                Prefab.MenuArea(new Rect(10, 436 - 125 - 10, 220, 125), "SPAMMER", () =>
+                {
+                    Prefab.Toggle("Enabled", ref MiscOptions.SpammerEnabled);
+                    GUILayout.Space(5);
+                    MiscOptions.SpamText = Prefab.TextField(MiscOptions.SpamText, "Text: ", 150);
+                    GUILayout.Space(10);
+                    GUILayout.Label("Delay: " + (int)MiscOptions.SpammerDelay + "ms", Prefab._TextStyle);
+                    GUILayout.Space(5);
+                    MiscOptions.SpammerDelay = (int)Prefab.Slider(0, 3000, MiscOptions.SpammerDelay, 175);
+                });
+                Prefab.MenuArea(new Rect(220 + 10 + 5, 436 - 125 - 10, 221, 125), "INTERACT", () =>
+                {
+                    Prefab.Toggle("Hit Structures", ref InteractionOptions.HitStructures);
+                    Prefab.Toggle("Hit Barricades", ref InteractionOptions.HitBarricades);
+                    Prefab.Toggle("Hit Items", ref InteractionOptions.HitItems);
+                    Prefab.Toggle("Hit Vehicles", ref InteractionOptions.HitVehicles);
+                    Prefab.Toggle("Hit Resources", ref InteractionOptions.HitResources);
+                });
+            });
         }
     }
 }
--- ThankingHack/Components/UI/Menu/Tabs/WeaponsTab.cs
+++ ThankingHack/Components/UI/Menu/Tabs/WeaponsTab.cs
@@ -1,4 +1,6 @@
-﻿using Thanking.Options.AimOptions;
+﻿using SDG.Unturned;
+using Thanking.Options.AimOptions;
+using Thanking.Options.UIVariables;
 using UnityEngine;
 
 namespace Thanking.Components.UI.Menu.Tabs
@@ -9,11 +11,106 @@ namespace Thanking.Components.UI.Menu.Tabs
         {
             Prefab.MenuArea(new Rect(0, 0, 466, 436), "WEAPONS", () =>
             {
-                Prefab.Toggle("Silent Aimbot", ref RaycastOptions.Enabled);
+                GUILayout.BeginHorizontal();
+                GUILayout.BeginVertical(GUILayout.Width(230));
                 Prefab.Toggle("Triggerbot", ref TriggerbotOptions.Enabled);
                 Prefab.Toggle("No Recoil", ref WeaponOptions.NoRecoil);
                 Prefab.Toggle("No Spread", ref WeaponOptions.NoSpread);
                 Prefab.Toggle("No Sway", ref WeaponOptions.NoSway);
+                GUILayout.Space(20);
+                Prefab.Toggle("Silent Aimbot", ref RaycastOptions.Enabled);
+                Prefab.Toggle("Random Limb", ref RaycastOptions.UseRandomLimb);
+                Prefab.Toggle("Custom Ragdoll Vector", ref RaycastOptions.UseModifiedVector);
+                Prefab.Toggle("Normal Material", ref RaycastOptions.UseTargetMaterial);
+                Prefab.Toggle("Extended Range", ref RaycastOptions.ExtendedRange);
+                GUILayout.Space(5);
+                GUILayout.Label("Sphere Radius: " + System.Math.Round(SphereOptions.SphereRadius, 2) + "m", Prefab._TextStyle);
+                Prefab.Slider(0, 16, ref SphereOptions.SphereRadius, 200);
+                GUILayout.Label("Vehicle Sphere Radius: " + System.Math.Round(SphereOptions.VehicleSphereRadius, 2) + "m", Prefab._TextStyle);
+                Prefab.Slider(0, 16, ref SphereOptions.VehicleSphereRadius, 200);
+                GUILayout.Label("Recursion Level: " + SphereOptions.RecursionLevel, Prefab._TextStyle);
+                SphereOptions.RecursionLevel = (int)Prefab.Slider(0, 4, SphereOptions.RecursionLevel, 200);
+                GUILayout.Space(20);
+
+                GUIContent[] TargetPriorities = new GUIContent[]
+                {
+                    new GUIContent("Player"),
+                    new GUIContent("Zombie"),
+                    new GUIContent("Sentry"),
+                    new GUIContent("Bed"),
+                    new GUIContent("Claim Flag"),
+                    new GUIContent("Storage")
+                };
+
+                if (Prefab.List(200, "_TargetPriority", new GUIContent("Priority: " + TargetPriorities[DropDown.Get("_TargetPriority").ListIndex].text), TargetPriorities))
+                {
+                    RaycastOptions.Target = (TargetPriority)DropDown.Get("_TargetPriority").ListIndex;
+                }
+
+                GUIContent[] Limbs = new GUIContent[]
+                {
+                    new GUIContent("Left Foot"),
+                    new GUIContent("Left Leg"),
+                    new GUIContent("Right Foot"),
+                    new GUIContent("Right Leg"),
+                    new GUIContent("Left Hand"),
+                    new GUIContent("Left Arm"),
+                    new GUIContent("Right Hand"),
+                    new GUIContent("Right Arm"),
+                    new GUIContent("Left Back"),
+                    new GUIContent("Right Back"),
+                    new GUIContent("Left Front"),
+                    new GUIContent("Right Front"),
+                    new GUIContent("Spine"),
+                    new GUIContent("Skull")
+                };
+
+                if (Prefab.List(200, "_TargetLimb", new GUIContent("Limb: " + Limbs[DropDown.Get("_TargetLimb").ListIndex].text), Limbs))
+                {
+                    RaycastOptions.TargetLimb = (ELimb)DropDown.Get("_TargetLimb").ListIndex;
+                }
+
+                GUIContent[] Materials = new GUIContent[]
+                {
+                    new GUIContent("None"),
+                    new GUIContent("Cloth Dynamic"),
+                    new GUIContent("Cloth Static"),
+                    new GUIContent("Tile Dynamic"),
+                    new GUIContent("Tile Static"),
+                    new GUIContent("Concrete Dynamic"),
+                    new GUIContent("Concrete Static"),
+                    new GUIContent("Flesh Dynamic"),
+                    new GUIContent("Gravel Dynamic"),
+                    new GUIContent("Gravel Static"),
+                    new GUIContent("Metal Dynamic"),
+                    new GUIContent("Metal Static"),
+                    new GUIContent("Metal Slip"),
+                    new GUIContent("Wood Dynamic"),
+                    new GUIContent("Wood Static"),
+                    new GUIContent("Foliage Static"),
+                    new GUIContent("Foliage Dynamic"),
+                    new GUIContent("Snow Static"),
+                    new GUIContent("Ice Static"),
+                    new GUIContent("Water Static"),
+                    new GUIContent("Alien Dynamic")
+                };
+
+                if (Prefab.List(200, "_TargetMaterial", new GUIContent("Material: " + Materials[DropDown.Get("_TargetMaterial").ListIndex].text), Materials))
+                {
+                    RaycastOptions.TargetMaterial = (EPhysicsMaterial)DropDown.Get("_TargetMaterial").ListIndex;
+                }
+                GUILayout.EndVertical();
+                GUILayout.BeginVertical();
+
+                GUILayout.Label("Ragdoll Vector: X: " + RaycastOptions.TargetRagdoll.x, Prefab._TextStyle);
+                RaycastOptions.TargetRagdoll.x = (int)Prefab.Slider(-25, 25, RaycastOptions.TargetRagdoll.x, 200);
+                GUILayout.Label("Ragdoll Vector: Y: " + RaycastOptions.TargetRagdoll.y, Prefab._TextStyle);
+                RaycastOptions.TargetRagdoll.y = (int)Prefab.Slider(-25, 25, RaycastOptions.TargetRagdoll.y, 200);
+                GUILayout.Label("Ragdoll Vector: Z: " + RaycastOptions.TargetRagdoll.z, Prefab._TextStyle);
+                RaycastOptions.TargetRagdoll.z = (int)Prefab.Slider(-25, 25, RaycastOptions.TargetRagdoll.z, 200);
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
             });
         }
     }
