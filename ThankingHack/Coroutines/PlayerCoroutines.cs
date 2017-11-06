0eaebe390c89ec01985cda29efbae5d8b9f57d24
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-11-05 20:19:06 -0600

    Major Spy Optimization, More Anti-Cancer

--- ThankingHack/Coroutines/PlayerCoroutines.cs
+++ ThankingHack/Coroutines/PlayerCoroutines.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using SDG.Unturned;
 using Thanking.Attributes;
 using Thanking.Components.UI;
+using Thanking.Managers.Submanagers;
 using Thanking.Utilities;
 using Thanking.Variables;
 using UnityEngine;
@@ -13,32 +14,23 @@ namespace Thanking.Coroutines
 {
 	public static class PlayerCoroutines
 	{
-        public static bool IsSpying;
+		public static bool IsSpying;
 
 		public static IEnumerator TakeScreenshot()
 		{
-            IsSpying = true;
+			IsSpying = true;
 
 			#if DEBUG
             DebugUtilities.Log("TAKING SCREENSHOT");
 			#endif
-			
-            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-                foreach (Type tClass in asm.GetTypes())
-                    foreach (MethodInfo tMethod in tClass.GetMethods(ReflectionVariables.Everything))
-                        if (tMethod.IsDefined(typeof(OnSpyAttribute), false))
-                            tMethod.Invoke(null, null);
 
-            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-				foreach (Type tClass in asm.GetTypes())
-					if (tClass.IsClass)
-						if (tClass.IsDefined(typeof(SpyComponentAttribute), false))
-							Object.Destroy(Loader.HookObject.GetComponent(tClass));
+			SpyManager.InvokePre();
+			SpyManager.DestroyComponents();
 
 			if (Player.player.equipment.asset is ItemGunAsset pAsset)
 			{
 				UseableGun PGun = Player.player.equipment.useable as UseableGun;
-				
+
 				PlayerUI.updateCrosshair(PGun.isAiming
 					? WeaponComponent.AssetBackups[pAsset.id][5]
 					: WeaponComponent.AssetBackups[pAsset.id][6]);
@@ -47,6 +39,7 @@ namespace Thanking.Coroutines
 			yield return new WaitForFixedUpdate();
 
 			#region Take Screenshot
+
 			yield return new WaitForEndOfFrame();
 			Texture2D screenshotRaw =
 				new Texture2D(Screen.width, Screen.height, (TextureFormat) 3, false)
@@ -54,7 +47,7 @@ namespace Thanking.Coroutines
 					name = "Screenshot_Raw",
 					hideFlags = (HideFlags) 61
 				};
-			
+
 			screenshotRaw.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0, false);
 
 			Texture2D screenshotFinal = new Texture2D(640, 480, (TextureFormat) 3, false)
@@ -65,48 +58,42 @@ namespace Thanking.Coroutines
 
 			Color[] oldColors = screenshotRaw.GetPixels();
 			Color[] newColors = new Color[screenshotFinal.width * screenshotFinal.height];
-			float widthRatio = screenshotRaw.width / (float)screenshotFinal.width;
-			float heightRatio = screenshotRaw.height / (float)screenshotFinal.height;
-			
+			float widthRatio = screenshotRaw.width / (float) screenshotFinal.width;
+			float heightRatio = screenshotRaw.height / (float) screenshotFinal.height;
+
 			for (int i = 0; i < screenshotFinal.height; i++)
 			{
-				int num = (int)(i * heightRatio) * screenshotRaw.width;
+				int num = (int) (i * heightRatio) * screenshotRaw.width;
 				int num2 = i * screenshotFinal.width;
 				for (int j = 0; j < screenshotFinal.width; j++)
 				{
-					int num3 = (int)(j * widthRatio);
+					int num3 = (int) (j * widthRatio);
 					newColors[num2 + j] = oldColors[num + num3];
 				}
 			}
-			
+
 			screenshotFinal.SetPixels(newColors);
 			byte[] data = screenshotFinal.EncodeToJPG(33);
-			
+
 			if (data.Length < 30000)
 			{
 				Player.player.channel.longBinaryData = true;
 				Player.player.channel.openWrite();
 				Player.player.channel.write(data);
-				Player.player.channel.closeWrite("tellScreenshotRelay", ESteamCall.SERVER, ESteamPacket.UPDATE_RELIABLE_CHUNK_BUFFER);
+				Player.player.channel.closeWrite("tellScreenshotRelay", ESteamCall.SERVER,
+					ESteamPacket.UPDATE_RELIABLE_CHUNK_BUFFER);
 				Player.player.channel.longBinaryData = false;
 			}
+
 			#endregion
 
 			yield return new WaitForFixedUpdate();
 
-			foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-				foreach (Type tClass in asm.GetTypes())
-					if (tClass.IsClass)
-						if (tClass.IsDefined(typeof(SpyComponentAttribute), false))
-							Loader.HookObject.AddComponent(tClass);
+			SpyManager.AddComponents();
 
-            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-                foreach (Type tClass in asm.GetTypes())
-                    foreach (MethodInfo tMethod in tClass.GetMethods(ReflectionVariables.Everything))
-                        if (tMethod.IsDefined(typeof(OffSpyAttribute), false))
-                            tMethod.Invoke(null, null);
+			SpyManager.InvokePost();
 
-                            IsSpying = false;
+			IsSpying = false;
 		}
 	}
 }
