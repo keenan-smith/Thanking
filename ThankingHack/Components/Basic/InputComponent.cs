b62ae3e1bbdcd3cffe75c1411b2a7c8664372e29
DefCon42 <klrehm123@gmail.com>
2017-11-07 20:24:49 -0500

    Removed panic button, reworked friends

--- ThankingHack/Components/Basic/InputComponent.cs
+++ ThankingHack/Components/Basic/InputComponent.cs
@@ -39,57 +39,6 @@ namespace Thanking.Components.Basic
 
 			if (Input.GetKeyDown(MiscOptions.SaveConfig))
 				ConfigManager.SaveConfig(ConfigManager.CollectConfig());
-
-			if (Input.GetKeyDown(MiscOptions.PanicButton))
-			{
-				MiscOptions.VisualsEnabled = !MiscOptions.VisualsEnabled;
-				if (MiscOptions.VisualsEnabled)
-				{
-					foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-						foreach (Type tClass in asm.GetTypes())
-							if (tClass.IsClass)
-								if (tClass.IsDefined(typeof(SpyComponentAttribute), false))
-									Loader.HookObject.AddComponent(tClass);
-				}
-				else
-				{
-					foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-						foreach (Type tClass in asm.GetTypes())
-							if (tClass.IsClass)
-								if (tClass.IsDefined(typeof(SpyComponentAttribute), false))
-									Destroy(Loader.HookObject.GetComponent(tClass));
-
-					if (Player.player.equipment.asset is ItemGunAsset)
-					{
-						ItemGunAsset PAsset = Player.player.equipment.asset as ItemGunAsset;
-						UseableGun PGun = Player.player.equipment.useable as UseableGun;
-						if (PGun.isAiming)
-							PlayerUI.updateCrosshair(WeaponComponent.AssetBackups[PAsset.id][5]);
-						else
-							PlayerUI.updateCrosshair(WeaponComponent.AssetBackups[PAsset.id][6]);
-					}
-				}
-			}
-
-			if (Input.GetKeyDown(KeyCode.KeypadPeriod))
-				foreach (SteamPlayer player in Provider.clients)
-				{
-					WalkingPlayerInputPacket packet = new WalkingPlayerInputPacket();
-					packet.analog = 0;
-					packet.keys = 0;
-					packet.pitch = 0;
-					packet.yaw = 0;
-					packet.clientsideInputs = null;
-					packet.serversideInputs = null;
-					packet.recov = int.MaxValue;
-					packet.sequence = int.MaxValue;
-					packet.position = new Vector3(0, 0, 0);
-
-					SteamChannel c = Player.player.channel;
-					c.openWrite();
-					packet.write(c);
-					c.closeWrite("askInput", player.playerID.steamID, ESteamPacket.UPDATE_RELIABLE_CHUNK_INSTANT);
-				}
 		}
 	}
 }
