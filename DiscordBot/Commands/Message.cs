258a022e41718e479a4dceb90990d3ead22590c8
Kr4ken-9 <kr4kenkt@gmail.com>
2017-11-09 23:58:34 -0600

    Reduce Autism

--- DiscordBot/Commands/Message.cs
+++ DiscordBot/Commands/Message.cs
@@ -8,7 +8,7 @@ namespace DiscordBot.Commands
     public class Message : ModuleBase
     {
         [Command("Message"), Summary("Messages designated user")]
-        public async Task AnnounceCMD([Summary("User to message")] IUser User, [Summary("Message to send")] String Message)
+        public async Task MessageCMD([Summary("User to message")] IUser User, [Summary("Message to send")] String Message)
             => await (await User.GetOrCreateDMChannelAsync()).SendMessageAsync(Message);
     }
 }
