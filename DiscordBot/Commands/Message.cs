d01944f12b60d0c9b4f948bb6f8f1bdbbb9cca07
Kr4ken-9 <kr4kenkt@gmail.com>
2017-11-09 23:57:31 -0600

    Discord Bot

---
+++ DiscordBot/Commands/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Commands
+{
+    public class Message : ModuleBase
+    {
+        [Command("Message"), Summary("Messages designated user")]
+        public async Task AnnounceCMD([Summary("User to message")] IUser User, [Summary("Message to send")] String Message)
+            => await (await User.GetOrCreateDMChannelAsync()).SendMessageAsync(Message);
+    }
+}
