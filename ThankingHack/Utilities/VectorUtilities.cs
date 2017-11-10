03f8e21a69d2af35c0ba2a901fc917e1b7ef1fc5
Kr4ken-9 <kr4kenkt@gmail.com>
2017-11-10 11:51:26 -0600

    Anti-Cancer

--- ThankingHack/Utilities/VectorUtilities.cs
+++ ThankingHack/Utilities/VectorUtilities.cs
@@ -6,6 +6,9 @@ namespace Thanking.Utilities
 {
     public static class VectorUtilities
     {
+        public static double GetDistance(Vector3 point) =>
+            GetDistance(MainCamera.instance.transform.position, point);
+        
         public static double GetDistance(Vector3 start, Vector3 point)
         {
             Vector3 heading;
@@ -14,18 +17,6 @@ namespace Thanking.Utilities
             heading.y = start.y - point.y;
             heading.z = start.z - point.z;
             
-            return Math.Sqrt(heading.x * heading.x + heading.y * heading.y + heading.z * heading.z);
-        }
-
-        public static double GetDistance(Vector3 point)
-        {
-            Vector3 start = MainCamera.instance.transform.position;
-            Vector3 heading;
-
-            heading.x = start.x - point.x;
-            heading.y = start.y - point.y;
-            heading.z = start.z - point.z;
-
             return Math.Sqrt(heading.x * heading.x + heading.y * heading.y + heading.z * heading.z);
         }
     }
