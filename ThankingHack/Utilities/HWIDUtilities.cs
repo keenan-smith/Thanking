3ab4db3656f7307d864b203dec4a08b319a96581
SDPlugins <admin@sdplugins.com>
2017-11-26 11:27:04 -0500

    Fix up HWIDUtilities

--- ThankingHack/Utilities/HWIDUtilities.cs
+++ ThankingHack/Utilities/HWIDUtilities.cs
@@ -113,10 +113,39 @@ namespace Thanking.Utilities
 		/// </summary>
 		VolumeQuotas = 0x20
 	}
-#endregion
+
+	[Flags]
+	public enum AllocationType : uint
+	{
+		COMMIT = 0x1000,
+		RESERVE = 0x2000,
+		RESET = 0x80000,
+		LARGE_PAGES = 0x20000000,
+		PHYSICAL = 0x400000,
+		TOP_DOWN = 0x100000,
+		WRITE_WATCH = 0x200000
+	}
+
+	[Flags]
+	public enum MemoryProtection : uint
+	{
+		EXECUTE = 0x10,
+		EXECUTE_READ = 0x20,
+		EXECUTE_READWRITE = 0x40,
+		EXECUTE_WRITECOPY = 0x80,
+		NOACCESS = 0x01,
+		READONLY = 0x02,
+		READWRITE = 0x04,
+		WRITECOPY = 0x08,
+		GUARD_Modifierflag = 0x100,
+		NOCACHE_Modifierflag = 0x200,
+		WRITECOMBINE_Modifierflag = 0x400
+	}
+	#endregion
 
 	public static class HWIDUtilities
 	{
+		#region Imports
 		[DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		public extern static bool GetVolumeInformation(
@@ -141,6 +170,7 @@ namespace Thanking.Utilities
 
 		[DllImport("kernel32")]
 		private static extern bool VirtualFree(IntPtr lpAddress, UInt32 dwSize, UInt32 dwFreeType);
+		#endregion
 
 		public static string GetHWID()
 		{
@@ -237,34 +267,6 @@ namespace Thanking.Utilities
 					codePointer = IntPtr.Zero;
 				}
 			}
-		}
-
-		[Flags()]
-		private enum AllocationType : uint
-		{
-			COMMIT = 0x1000,
-			RESERVE = 0x2000,
-			RESET = 0x80000,
-			LARGE_PAGES = 0x20000000,
-			PHYSICAL = 0x400000,
-			TOP_DOWN = 0x100000,
-			WRITE_WATCH = 0x200000
-		}
-
-		[Flags()]
-		private enum MemoryProtection : uint
-		{
-			EXECUTE = 0x10,
-			EXECUTE_READ = 0x20,
-			EXECUTE_READWRITE = 0x40,
-			EXECUTE_WRITECOPY = 0x80,
-			NOACCESS = 0x01,
-			READONLY = 0x02,
-			READWRITE = 0x04,
-			WRITECOPY = 0x08,
-			GUARD_Modifierflag = 0x100,
-			NOCACHE_Modifierflag = 0x200,
-			WRITECOMBINE_Modifierflag = 0x400
 		}
 
 		// Basic ASM strategy --
