1bdf6de729c3d76eb5043404f297c1b0f0846120
DefCon42 <klrehm123@gmail.com>
2017-11-28 15:53:59 -0500

    fucking hell auto item pickup is cancer

--- ThankingHack/Components/Basic/ItemsComponent.cs
+++ ThankingHack/Components/Basic/ItemsComponent.cs
@@ -10,14 +10,14 @@ namespace Thanking.Components.Basic
     public class ItemsComponent : MonoBehaviour
     {
         public static List<ItemAsset> items = new List<ItemAsset>();
-        public static int uAmount = 100000;
+        public static ushort uAmount = ushort.MaxValue;
 
         public static void RefreshItems()
         {
             items.Clear();
-            for (int i = 0; i < uAmount; i++)
+            for (ushort i = 0; i < uAmount; i++)
             {
-                ItemAsset asset = (ItemAsset)Assets.find(EAssetType.ITEM, (ushort)i);
+                ItemAsset asset = (ItemAsset)Assets.find(EAssetType.ITEM, i);
 
                 if (!string.IsNullOrEmpty(asset?.itemName) && !items.Contains(asset))
                     items.Add(asset);
