2fdc04d2a8e04b69c6917f4dd82b4c09eaa5233f
Keenan Smith <01573187@student.commnet.edu>
2017-12-01 18:16:12 -0500

    started master server

---
+++ ThankingProcessing/ThankingProcessing/Models/DownloadRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThankingProcessing.Models
+{
+    public class DownloadRequest
+    {
+        public string stage { get; set; }
+        public string hwid { get; set; }
+        public string steamname { get; set; }
+        public string steam64 { get; set; }
+    }
+}
