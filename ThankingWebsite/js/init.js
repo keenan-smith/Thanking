902dcce874c4e434dbe20ac9db4cdd4a4b3373b6
DefCon42 <klrehm123@gmail.com>
2017-12-01 15:04:07 -0500

    more work on website (fuck you zoomy it looks decent)

--- ThankingWebsite/js/init.js
+++ ThankingWebsite/js/init.js
@@ -1,20 +1,51 @@
 (function($){
     $(function(){
-        $('.button-collapse').sideNav();
-        $('#download-button').click(function() {
+        if (document.cookie.indexOf("meme") != -1) {
             $('#thanking-img').css("max-width", "");
             $('#thanking-img').css("max-height", "");
             $('#thanking-img').css("min-width", $('#thanking-img').width() + "px")
             $('#thanking-img').css("min-height", $('#thanking-img').height() + "px");
-		
-            $("#before").animate({ 
-                height: 0, 
-                opacity: 0,
-                margin: 0,
-                padding: 0
-            }, 'slow', function(){
-                $(this).hide();
-            }); 
-        })
-    }); // end of document ready
+            $("#before").delay(500).animate({ 
+                   height: 0, 
+                   opacity: 0,
+                   margin: 0,
+                   padding: 0
+               }, 'slow', function(){
+                   $(this).hide();
+                   $(".hidden").each (function(i) {
+                       var x = this;
+                       setTimeout(function() {
+                           $(x).fadeIn(1500).css('display','inline-block').removeClass('hidden');
+                       }, 300 * i);
+                });
+                   
+                $('#after').show();
+            });
+        }
+        else {
+            document.cookie = "meme;";
+            $('#download-button').click(function() {
+               $('#thanking-img').css("max-width", "");
+               $('#thanking-img').css("max-height", "");
+               $('#thanking-img').css("min-width", $('#thanking-img').width() + "px")
+               $('#thanking-img').css("min-height", $('#thanking-img').height() + "px");
+                   $("#before").animate({ 
+                   height: 0, 
+                   opacity: 0,
+                   margin: 0,
+                   padding: 0
+               }, 'slow', function(){
+                   $(this).hide();
+                   $(".hidden").each (function(i) {
+                       var x = this;
+                       setTimeout(function() {
+                           $(x).fadeIn(1500).css('display','inline-block').removeClass('hidden');
+                       }, 300 * i);
+                   });
+                   
+                   $('#after').show();
+               }); 
+            });   
+        }
+    }); // end of document ready)
 })(jQuery); // end of jQuery name space
