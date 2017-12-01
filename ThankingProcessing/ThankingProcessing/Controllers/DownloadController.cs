82eb154e6b96b895a67a97bb08daa613a5485d6e
Keenan Smith <01573187@student.commnet.edu>
2017-12-01 18:16:12 -0500

    started master server

---
+++ ThankingProcessing/ThankingProcessing/Controllers/DownloadController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using ThankingProcessing.Models;
+
+namespace ThankingProcessing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string GetDocumentContents(Microsoft.AspNetCore.Http.HttpRequest Request)
+        {
+            string documentContents;
+            using (Stream receiveStream = Request.Body)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    documentContents = readStream.ReadToEnd();
+                }
+            }
+            return documentContents;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Error: No Data Input.";
+            return View();
+        }
+
+        [HttpPost]
+        [Route("download")]
+        public string download(DownloadRequest content)
+        {
+            return content.stage;
+        }
+    }
+}
