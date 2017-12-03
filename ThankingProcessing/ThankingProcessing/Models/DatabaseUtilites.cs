d33711ee8838b319f3ffbd1a737d213ce24753a8
Unknown <Kr4kenKT@gmail.com>
2017-12-02 19:08:45 -0600

    MySQL Reading

--- ThankingProcessing/ThankingProcessing/Models/DatabaseUtilites.cs
+++ ThankingProcessing/ThankingProcessing/Models/DatabaseUtilites.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ThankingProcessing.Models
 {
@@ -10,10 +11,34 @@ namespace ThankingProcessing.Models
     {
         public async Task<UserObject> GetUser(string HWID)
         {
-            using (var db = new AppDb(Startup.database))
+            UserObject User = new UserObject();
+            
+            using (AppDb db = new AppDb(Startup.database))
             {
+                using (MySqlCommand Command = db.Connection.CreateCommand())
+                {
+                    Command.Parameters.AddWithValue("HWID", HWID);
+                    Command.CommandText = "SELECT * FROM Users WHERE HWID = @HWID";
 
+                    await db.Connection.OpenAsync();
+
+                    using (MySqlDataReader Reader = Command.ExecuteReader())
+                    {
+                        while (await Reader.ReadAsync())
+                        {
+                            User.Hwid = HWID;
+                            User.Ip = Reader.GetString(2);
+                            User.IsBlacklisted = Reader.GetBoolean(3);
+                            User.IsPremium = Reader.GetBoolean(4);
+                            User.Steam64 = Reader.GetInt64(5);
+                            User.SteamName = Reader.GetString(6);
+                            User.LastUse = Reader.GetDateTime(7);
+                        }
+                    }
+                }
             }
+
+            return User;
         }
 
         public async Task<UserObject[]> ReadAllAsync(DbDataReader reader)
