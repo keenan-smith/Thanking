394c56b489058f577d66c02b316fe51394756a67
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-12-03 21:02:54 -0600

    Dee-Bug

--- ThankingHack/Managers/Main/NetManager.cs
+++ ThankingHack/Managers/Main/NetManager.cs
@@ -4,6 +4,7 @@ using SDG.Unturned;
 using Steamworks;
 using Thanking.Attributes;
 using Thanking.Options.VisualOptions;
+using Thanking.Utilities;
 
 namespace Thanking.Managers.Main
 {
@@ -17,18 +18,30 @@ namespace Thanking.Managers.Main
         public static NetManager Manager;
 
         // Ask client if they have hacks
-        public void sendHasHacks() => 
+        public void sendHasHacks()
+        {
+            DebugUtilities.Log("Sending has hacks");
+            
             channel.send("getHasHacks", ESteamCall.PEERS, ESteamPacket.UPDATE_RELIABLE_BUFFER);
+        }
 
         // Listen for hack query, respond confirming hacks
         [SteamCall]
-        public void getHasHacks(CSteamID steamID) =>
+        public void getHasHacks(CSteamID steamID)
+        {
+            DebugUtilities.Log("Getting has hacks");
+            
             channel.send("getConfirmHack", steamID, ESteamPacket.UPDATE_RELIABLE_BUFFER);
+        }
 
         // Listen for hack confirmation, add to Heccers if confirmed
         [SteamCall]
-        public void getConfirmHack(CSteamID steamID) => 
+        public void getConfirmHack(CSteamID steamID)
+        {
+            DebugUtilities.Log("Confirming has hacks");
+            
             Heccers = Heccers?.Concat(new [] {steamID.m_SteamID}).ToArray() ?? new[] {steamID.m_SteamID};
+        }
 
         // Create a new instance of NetManager
         [Initializer]
