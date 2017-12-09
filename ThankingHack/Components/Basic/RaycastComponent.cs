4623873734c030e294537c1262d70b1ce2b2b2c9
DefCon42 <klrehm123@gmail.com>
2017-12-09 09:49:22 -0500

    SilentAim predictive movement

--- ThankingHack/Components/Basic/RaycastComponent.cs
+++ ThankingHack/Components/Basic/RaycastComponent.cs
@@ -14,14 +14,16 @@ namespace Thanking.Components.Basic
     public class RaycastComponent : MonoBehaviour
     {
         private Vector3 prevPos = Vector3.zero;
+        private Vector3 Velocity = Vector3.zero;
+        private Vector3 PredictedVelocity = Vector3.zero;
         public GameObject Sphere;
         public float Speed = -1;
         public float Radius = -1;
 
         void Awake()
         {
-            StartCoroutine(CalcVelocity());
             StartCoroutine(CalcSphere());
+            StartCoroutine(CalcVelocity());
         }
   
         IEnumerator CalcVelocity()
@@ -29,8 +31,12 @@ namespace Thanking.Components.Basic
             while(true)
             {
                 prevPos = transform.position;
-                yield return new WaitForSeconds(0.5f);
-                Speed = (float) VectorUtilities.GetDistance(prevPos, transform.position) * 2;
+                yield return new WaitForSeconds(0.25f);
+                
+                Velocity = (transform.position - prevPos) * 4;
+                Speed = (float)VectorUtilities.GetMagnitude(Velocity);
+                
+                Sphere.transform.localPosition = PredictedVelocity;
             }
         }
 
@@ -42,13 +48,11 @@ namespace Thanking.Components.Basic
             
             while (true)
             {
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(0.25f);
                 
                 SetRadius();
-                
                 Destroy(Sphere);
                 Sphere = IcoSphere.Create("HitSphere", Radius, SphereOptions.RecursionLevel);
-                
                 SetUpSphere();
             }
         }
@@ -57,16 +61,19 @@ namespace Thanking.Components.Basic
         {
             Speed = SphereOptions.DynamicSphere ? Speed : -1;
             Radius = SphereOptions.SphereRadius;
-			float Magnitude = Speed * Provider.ping * 1.1f;
 
             if (Speed > 0)
-                Radius = 15.5f - Magnitude;
+            {
+                PredictedVelocity = VectorUtilities.Normalize(Velocity) * Speed * Provider.ping;
+                float MarginOfError = Provider.ping * 1.1f;
+                
+                Radius = 15.8f - MarginOfError;
+            }
         }
 
         void SetUpSphere()
         {
             Sphere.transform.parent = transform;
-            Sphere.transform.localPosition = Vector3.zero;
             Sphere.layer = LayerMasks.AGENT;
         }
     }
