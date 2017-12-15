6f56b7b4312abd1a4925a403e52d240ecaaf4591
Ice Wolf <ic3w0lf22@gmail.com>
2017-12-15 14:38:43 -0700

    Added Skin Changer + RQ + Fixed white gun/item thing + idk what else i fixed/broke+more bugs

---
+++ ThankingHack/Components/UI/Menu/Tabs/SkinsTab.cs
@@ -0,0 +1,27 @@
+﻿using SDG.Unturned;
+using System.Collections.Generic;
+using System.Linq;
+using Thanking.Options;
+using Thanking.Options.UIVariables;
+using Thanking.Options.VisualOptions;
+using Thanking.Utilities;
+using UnityEngine;
+
+namespace Thanking.Components.UI.Menu.Tabs
+{
+    public static class SkinsTab
+    {
+        public static void Tab()
+        {
+            SkinsUtilities.DrawSkins(SkinOptions.SkinWeapons);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesShirts);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesPants);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesBackpack);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesHats);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesMask);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesVest);
+            SkinsUtilities.DrawSkins(SkinOptions.SkinClothesGlasses);
+            GUILayout.Label("Don't get too excited... All skins are client-sided meaning other's\ncan't see them! Re-equip your weapon to apply the skin.", Prefab._TextStyle);
+        }
+    }
+}
---
+++ ThankingHack/Options/SkinOptions.cs
@@ -0,0 +1,82 @@
+﻿using SDG.Provider;
+using SDG.Unturned;
+using System.Collections.Generic;
+using Thanking.Attributes;
+using Thanking.Misc;
+using UnityEngine;
+
+namespace Thanking.Options
+{
+    public class Skin
+    {
+        public string Name;
+        public int ID;
+
+        public Skin(string Name, int ID)
+        {
+            this.Name = Name;
+            this.ID = ID;
+        }
+    }
+
+    public class WeaponSave
+    {
+        public ushort WeaponID;
+        public int SkinID;
+
+        public WeaponSave(ushort WeaponID, int SkinID)
+        {
+            this.WeaponID = WeaponID;
+            this.SkinID = SkinID;
+        }
+    }
+
+    public class SkinOptionList
+    {
+        public ESkinType Type = ESkinType.WEAPONS;
+        public HashSet<Skin> Skins = new HashSet<Skin>();
+
+        public SkinOptionList(ESkinType Type)
+        {
+            this.Type = Type;
+        }
+    }
+
+    public static class SkinOptions
+    {
+        [Save] public static SkinConfig SkinConfig = new SkinConfig();
+        public static SkinOptionList SkinWeapons = new SkinOptionList(ESkinType.WEAPONS);
+        public static SkinOptionList SkinClothesShirts = new SkinOptionList(ESkinType.SHIRTS);
+        public static SkinOptionList SkinClothesPants = new SkinOptionList(ESkinType.PANTS);
+        public static SkinOptionList SkinClothesBackpack = new SkinOptionList(ESkinType.BACKPACKS);
+        public static SkinOptionList SkinClothesVest = new SkinOptionList(ESkinType.VESTS);
+        public static SkinOptionList SkinClothesHats = new SkinOptionList(ESkinType.HATS);
+        public static SkinOptionList SkinClothesMask = new SkinOptionList(ESkinType.MASKS);
+        public static SkinOptionList SkinClothesGlasses = new SkinOptionList(ESkinType.GLASSES);
+    }
+
+    public class SkinConfig
+    {
+        public HashSet<WeaponSave> WeaponSkins = new HashSet<WeaponSave>();
+        public int ShirtID;
+        public int PantsID;
+        public int BackpackID;
+        public int VestID;
+        public int HatID;
+        public int MaskID;
+        public int GlassesID;
+    }
+
+    public enum ESkinType
+    {
+        NONE,
+        WEAPONS,
+        SHIRTS,
+        PANTS,
+        BACKPACKS,
+        VESTS,
+        HATS,
+        MASKS,
+        GLASSES
+    }
+}
