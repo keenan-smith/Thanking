e90fd396d68df7a2682374460000137db9c140d4
DefCon42 <klrehm123@gmail.com>
2018-02-09 12:26:54 -0500

    quick fix for unity autism (rigidbody mesh collider bs)

--- ThankingHack/Utilities/Mesh Utilities/IcoSphere.cs
+++ ThankingHack/Utilities/Mesh Utilities/IcoSphere.cs
@@ -169,7 +169,9 @@ namespace Thanking.Utilities.Mesh_Utilities
             mesh.normals = normales;
 
             mesh.RecalculateBounds();
+            
             sphere.AddComponent<MeshCollider>().sharedMesh = mesh;
+            sphere.GetComponent<MeshCollider>().convex = true;
             
             return sphere;
         }
