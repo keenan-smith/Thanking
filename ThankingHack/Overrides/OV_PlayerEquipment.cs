8f8e233d9b7d2f3b16de907507fbc51c5e41bb24
DefCon42 <klrehm123@gmail.com>
2018-07-17 00:18:34 -0400

    punch silent aim lol

---
+++ ThankingHack/Overrides/OV_PlayerEquipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using SDG.Unturned;
+using Thanking.Attributes;
+using Thanking.Options;
+using Thanking.Utilities;
+using Thanking.Variables;
+
+namespace Thanking.Overrides
+{
+    public class OV_PlayerEquipment
+    {
+        [Override(typeof(PlayerEquipment), "punch", BindingFlags.NonPublic | BindingFlags.Instance)]
+        public void OV_punch(EPlayerPunch p)
+        {
+            if (MiscOptions.PunchSilentAim)
+                OV_DamageTool.OVType = OverrideType.PlayerHit;
+            
+            OverrideUtilities.CallOriginal(OptimizationVariables.MainPlayer.equipment, p);
+            
+            OV_DamageTool.OVType = OverrideType.None;
+        }
+    }
+}
