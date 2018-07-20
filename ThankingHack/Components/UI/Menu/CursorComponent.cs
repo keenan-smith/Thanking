17babc1ee34e13d12cbedf373f28c0cd5987b1d1
SDPlugins <admin@sdplugins.com>
2018-07-20 13:48:40 -0400

    fix cursor meme i think

---
+++ ThankingHack/Components/UI/Menu/CursorComponent.cs
@@ -0,0 +1,30 @@
+﻿using SDG.Unturned;
+using Thanking.Attributes;
+using UnityEngine;
+
+namespace Thanking.Components.UI.Menu
+{
+    [SpyComponent]
+    [Component]
+    public class CursorComponent : MonoBehaviour
+    {
+        private Rect _cursor = new Rect(0, 0, 20f, 20f);
+        private Texture _cursorTexture;
+        
+        public void OnGUI()
+        {
+            if (MenuComponent.IsInMenu)
+            {
+                GUI.depth = -2;
+            
+                if (_cursorTexture == null)
+                    _cursorTexture = Resources.Load("UI/Cursor") as Texture;
+
+                PlayerUI.window.showCursor = true;
+                _cursor.x = Input.mousePosition.x;
+                _cursor.y = Screen.height - Input.mousePosition.y;
+                GUI.DrawTexture(_cursor, _cursorTexture);
+            }
+        }
+    }
+}
--- ThankingHack/Components/UI/Menu/MenuComponent.cs
+++ ThankingHack/Components/UI/Menu/MenuComponent.cs
@@ -30,8 +30,6 @@ namespace Thanking.Components.UI.Menu
         public static Color32 _Accent1;
         public static Color32 _Accent2;
 
-        private Rect _cursor = new Rect(0, 0, 20f, 20f);
-        private Texture _cursorTexture;
         private int _pIndex = 0;
 
         // Use this for initialization
@@ -66,25 +64,10 @@ namespace Thanking.Components.UI.Menu
         {
             if (IsInMenu && _LogoTexLarge != null)
             {
-                if (PlayerUI.window != null)
-                    PlayerUI.window.showCursor = true;
-
-                try
-                {
-                    if (_cursorTexture == null)
-                        _cursorTexture = Resources.Load("UI/Cursor") as Texture;
-
-                    PlayerUI.window.showCursor = true;
-                    _cursor.x = Input.mousePosition.x;
-                    _cursor.y = Screen.height - Input.mousePosition.y;
-                    GUI.depth = 0;
-                    GUI.DrawTexture(_cursor, _cursorTexture);
-                }
-                catch { }
+                GUI.depth = -1;
                 MenuRect = GUI.Window(0, MenuRect, DoMenu, "Ironic");
             }
         }
-
 
         #region Backend
         void DoMenu(int id)
