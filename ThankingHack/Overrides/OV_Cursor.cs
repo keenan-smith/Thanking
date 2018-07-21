66bc02cc556e20fc4327e004b00b4dddaf9e142e
SDPlugins <admin@sdplugins.com>
2018-07-20 21:23:40 -0400

    also fix tracers woo

--- ThankingHack/Overrides/OV_Cursor.cs
+++ ThankingHack/Overrides/OV_Cursor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using Thanking.Attributes;
 using Thanking.Components.UI.Menu;
+using Thanking.Coroutines;
 using UnityEngine;
 
 namespace Thanking.Overrides
@@ -11,7 +12,7 @@ namespace Thanking.Overrides
         
         [Override(typeof(Cursor), "get_lockState", BindingFlags.Public | BindingFlags.Static)]
         public static CursorLockMode OV_get_lockState() => 
-            MenuComponent.IsInMenu ? CursorLockMode.None : RealMode;
+            MenuComponent.IsInMenu && !PlayerCoroutines.IsSpying ? CursorLockMode.None : RealMode;
 
         [Override(typeof(Cursor), "set_lockState", BindingFlags.Public | BindingFlags.Static)]
         public static void OV_set_lockState(CursorLockMode mode) =>
