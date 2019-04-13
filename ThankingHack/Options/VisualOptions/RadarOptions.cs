796301107f390249fe4bce2df7c8c8cde409466e
Coopyy <coopyy1@gmail.com>
2019-04-13 15:52:42 -0500

    detiled radar players

--- ThankingHack/Options/VisualOptions/RadarOptions.cs
+++ ThankingHack/Options/VisualOptions/RadarOptions.cs
@@ -6,7 +6,7 @@ namespace Thanking.Options.VisualOptions
     {
         [Save] public static bool Enabled = false;
         [Save] public static int type = 3;
-        [Save] public static bool TrackPlayer = false;
+        [Save] public static bool DetialedPlayers = false;
         [Save] public static bool ShowPlayers = false;
         [Save] public static bool ShowVehicles = false;
         [Save] public static bool ShowVehiclesUnlocked = false;
